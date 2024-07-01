a5182214f5314e08b9a442cf8c2e0b3e8e8f2612
oyintanda-zongwana <oyintandazongone@gmail.com>
2024-07-01 09:54:46 +0200

    first commit

---
+++ src/components/NavbarComp.vue
@@ -0,0 +1,66 @@
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+            <nav class="navbar bg-body-tertiary">
+                <div class="container" >
+                  <a class="navbar-brand" href="./index.html">
+                    <img src="https://oyintanda-zongwana.github.io/hosted-pics/pics%20folder/brand.png" class="logo" alt="..." width="50" height="50">
+                  </a>
+                </div>
+            </nav>
+        </div>
+    
+
+    <nav class="navbar bg-dark d-flex justify-content-between" >
+        <!-- Navbar content -->
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+              <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link"  href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">About</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Resume</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Projects</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Testimonials</a>
+                    
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link" href="#">Contact</a>
+                </li>
+              </ul>
+            </div>
+    </nav>
+    </nav>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style scoped>
+.logo {
+    background: white;
+    border-radius: 35%;
+} 
+.navbar {
+    position: sticky;
+    top: 0;
+    z-index: 1;
+}
+#navbarSupportedContent li a i {
+    font-size: 20em;
+}
+#navbarSupportedContent li a:hover {
+    color: #29829b;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,54 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <section id="navbar">
+    <NavbarComp/>
+  </section>
+  <section id="landingPage">
+    <LandingPage/>
+  </section>
+
+
+    
+
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import LandingPage from '@/components/LandingPageComp.vue'
+import NavbarComp from '@/components/NavbarComp.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    LandingPage,
+    NavbarComp
   }
 }
 </script>
+<style scoped>
+@import url('https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap');
+* {
+    margin: 0;
+    padding: 0;
+    box-sizing: content-box;
+    overflow-x: hidden;
+    font-family: "League Spartan", sans-serif;
+}
+h1 {    font-size: 50px;
+    line-height: 64px;
+    color: #282525;
+}
+h2 {
+    font-size: 46px;
+    line-height: 54px;
+    color: #282525;
+}
+h4 {
+    font-size: 20px;
+    color: #282525;
+}
+h6 {
+    font-size: 12px;
+    font-weight: 700;
+}
+
+</style>
