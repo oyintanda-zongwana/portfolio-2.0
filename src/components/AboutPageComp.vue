ddbbf17368b2a0b3a3529a7cb8667c289d7a0b24
oyintanda-zongwana <oyintandazongone@gmail.com>
2024-07-01 12:40:25 +0200

    added skills

--- src/components/AboutPageComp.vue
+++ src/components/AboutPageComp.vue
@@ -3,7 +3,17 @@
         <img src="" alt="...">
         <div class="aboutme">
             <h1>About Me</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus vero nihil doloremque, reprehenderit unde nam earum quod aut recusandae, culpa eos excepturi quaerat laudantium saepe alias iusto fugiat necessitatibus tenetur?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. <br>Repellendus vero nihil doloremque, reprehenderit unde nam earum quod aut recusandae, culpa eos excepturi quaerat<br> laudantium saepe alias iusto fugiat necessitatibus tenetur?</p>
+        </div>
+        <div class="skills">
+            <h1>Skills Acquired</h1>
+            <div class="icons">
+                <i class="lab la-bootstrap"></i>
+                <i class="lab la-css3-alt"></i>
+                <i class="lab la-js-square"></i>
+                <i class="lab la-vuejs"></i>
+                <i class="lab la-html5"></i>
+            </div>
         </div>
     </div>
 </template>
