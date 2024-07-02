b0fb63f8f13a9b29892aee672e81f047d865b492
oyintanda-zongwana <oyintandazongone@gmail.com>
2024-07-02 16:21:07 +0200

    "Update landing page profile section: changed 'I'm an Aspiring Fullstack Developer' to 'An Aspiring Fullstack Developer'"

--- src/components/LandingPageComp.vue
+++ src/components/LandingPageComp.vue
@@ -7,7 +7,7 @@
         <div class="profile">
           <img src="https://oyintanda-zongwana.github.io/portfolioimgages/images/pic2.jpg" alt="ME" width="350em" height="400em">
           <h1>Hi, I'm Oyintanda Zongwana</h1>
-          <h4>I'm an Aspiring Fullstack Developer</h4>
+          <h4>An Aspiring Fullstack Developer</h4>
         </div>
         <div class="social">
           <a href="https://github.com/oyintanda-zongwana" target="_blank">
